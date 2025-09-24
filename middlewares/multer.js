1141a172b08ab17a106fe44ec591076528ef66a0
MauroBorzi <mauroborzi91@gmail.com>
2025-09-24 16:23:33 +0200

    fix multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 const storage = multer.diskStorage({
   destination: "./public/img/movies",
   filename: (req, file, cb) => {
-    const uniqueName = `${DataTransfer.now()}-${file.originalname}`
+    const uniqueName = `${Date.now()}-${file.originalname}`
     cb(null, uniqueName)
   }
 })
